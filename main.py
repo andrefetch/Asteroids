5a35944e1301e3ce35826c34359128a9236bf363
andrefetch <andresilvapersonal2007@gmail.com>
2026-05-13 17:46:39 -0400

    Drew the player onto the screen by inheriting from a shape class.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
 
     pygame.init()
+    Clock = pygame.time.Clock()
+
+    dt = 0
 
     VERSION = pygame.version.ver
 
@@ -12,6 +16,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    x = SCREEN_WIDTH/ 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
 
     while True:
         log_state()
@@ -21,7 +30,10 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        Second = Clock.tick(60)
+        dt = Second / 1000
 
 
 if __name__ == "__main__":
